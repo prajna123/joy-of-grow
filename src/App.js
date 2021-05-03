2cbfcdf64307a8bb9ab574b266c78cc86a0c737a
prajna123 <tupur360@gmail.com>
2021-05-03 14:47:12 +0530

    Added new files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Cart from './containers/Cart';
+import ProductList from './containers/ProductList';
+import BackgroundImage from './containers/BackgroundImage';
+import logo from './containers/logo.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="container">
+            <div className="row-centered">
+                 <button className='btn btn-primary '>Contact Us</button>
+            </div>
+            <div className="row">
+                <div className="col-md-12">
+                <BackgroundImage></BackgroundImage>
+                </div>
+            </div>
+            
+            <div className="row">
+                <div className="col-md-12">
+                    <ProductList />
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { combineReducers, createStore } from 'redux';
+import cartReducer from './ducks/cart';
+import productsReducer from './ducks/products';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import productsData from './data/products';
+import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const rootReducer = combineReducers({
+    cart: cartReducer,
+    products: productsReducer
+});
+
+let store = createStore(
+    rootReducer,
+    {
+        products: productsData // initial store values
+    },
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // for debugging
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
